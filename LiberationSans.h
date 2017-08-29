dc51d3e3a6abcb183aba8c97fc2593ba33ea4044
Matt Jenkins <matt@majenko.co.uk>
2017-08-29 22:17:16 +0100

    Initial import

---
+++ LiberationSans.h
@@ -0,0 +1,19 @@
+#ifndef _LIBERATIONSANS_H
+#define _LIBERATIONSANS_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t LiberationSans10[];
+    extern const uint8_t LiberationSans12[];
+    extern const uint8_t LiberationSans14[];
+    extern const uint8_t LiberationSans16[];
+    extern const uint8_t LiberationSans18[];
+    extern const uint8_t LiberationSans20[];
+    extern const uint8_t LiberationSans22[];
+    extern const uint8_t LiberationSans24[];
+    extern const uint8_t LiberationSans26[];
+    extern const uint8_t LiberationSans28[];
+    extern const uint8_t LiberationSans30[];
+};
+#endif
